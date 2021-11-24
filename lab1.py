50f92fa879966429c42e5b8f77151b9165ea7236
ashaa9121 <ashaa9121@gmail.com>
2021-11-24 22:48:50 +0530

    possibble strings

---
+++ lab1.py
@@ -0,0 +1,7 @@
+# Program:  To create all possible strings by using 'a', 'e', 'i', 'o', 'u'. 
+# Use the characters exactly once.
+
+import random
+a = ['a','e','i','o','u']
+random.shuffle(a)
+print(''.join(a))
