e4d571a611c3807df146c253785be4384ba83c99
ashaa9121 <ashaa9121@gmail.com>
2021-11-24 22:52:51 +0530

    length of string

---
+++ lab3.py
@@ -0,0 +1,9 @@
+# Program : To calculate the length of a string.
+
+input_string = input("Enter a string:")
+
+count = 0
+
+for i in input_string:
+    count = count+1
+print("Length of the string is: {}".format(count))    
