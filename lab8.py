0b692cafde2985f353d096b3613f471629e7c590
ashaa9121 <ashaa9121@gmail.com>
2021-11-25 15:10:05 +0530

    tuble elements

---
+++ lab8.py
@@ -0,0 +1,9 @@
+#Program : To get the 4th element and 4th element from last of a tuple.
+
+my_tuble = ('p','y','t','h','o','n')
+
+print("Tuble is :{}".format(my_tuble))
+print()
+print("4th element is : {}".format(my_tuble[3]))
+print()
+print("4th element from last is : {}".format(my_tuble[-4]))
