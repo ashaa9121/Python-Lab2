b337741377cf80265b9fce0a9743d513703a0134
ashaa9121 <ashaa9121@gmail.com>
2021-11-25 23:42:53 +0530

    remove an item from set

---
+++ lab14.py
@@ -0,0 +1,14 @@
+#Program : Write a Python program to remove an item from a set if it is present in the set.
+
+
+set_elements = ([1,2,3,4,5,6])
+print("Original set elements are :")
+print(set_elements)
+
+print("Element 4 remove from the set :")
+set_elements.remove(4)
+print(set_elements)
+
+print("Element 3 remove from set :")
+set_elements.remove(3)
+print(set_elements)
