b9ba735fd15e9edb8ff2c698f4361c48ea237865
ashaa9121 <ashaa9121@gmail.com>
2021-11-25 15:59:12 +0530

    reverse a tuple

---
+++ lab10.py
@@ -0,0 +1,12 @@
+#Program : Write a Python program to reverse a tuple.
+
+
+my_tuple = ('p','y','t','h','o','n')
+
+reverse = reversed(my_tuple)
+print(tuple(reverse))
+
+
+#slicing.....
+
+#print(my_tuple[::-1])
