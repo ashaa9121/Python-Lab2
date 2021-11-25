7256dd582d5cdcee1df66cf7f8aa1959c8d4d2db
ashaa9121 <ashaa9121@gmail.com>
2021-11-25 15:32:22 +0530

    index of tuple elements

---
+++ lab9.py
@@ -0,0 +1,10 @@
+#Program : Find the index of an item of a tuple.
+
+my_tuple = ('a','b','c','a','i','m','e')
+
+#index of 'c'
+print("Index of c : ",my_tuple.index('c'))
+
+# Index of 'a' after 1st index
+print("Index of 'a' after 1st index is :",my_tuple.index('a',1))
+
