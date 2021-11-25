44328fee2f4f53dcc45b3ac93f934ab5400dbed8
ashaa9121 <ashaa9121@gmail.com>
2021-11-25 16:25:03 +0530

    sum of items in list

---
+++ lab11.py
@@ -0,0 +1,10 @@
+#Program : Write a Python program to sum all the items in a list.
+
+my_list = [1,3,5,11,10]
+
+sum = 0
+for i in my_list:
+    sum = sum + i
+
+print("Sum of the list items is :",sum)    
+
