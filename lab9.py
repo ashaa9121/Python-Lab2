ae033f9b9fe65ed0bdeb297013508e0f5794f51f
ashaa9121 <ashaa9121@gmail.com>
2021-11-25 15:39:08 +0530

    tuple index

--- lab9.py
+++ lab9.py
@@ -8,3 +8,12 @@ print("Index of c : ",my_tuple.index('c'))
 # Index of 'a' after 1st index               result:3 
 print("Index of 'a' after 1st index is :",my_tuple.index('a',1))
 
+
+# tuple_1 = ('a','b','c','d','e','f')
+#print(tuple_1)
+#string = input("Enter the char want to check:")
+#try:
+#	index = tuple_1.index(string)
+#	print("Index of",string,"is",index)
+#except Exception as e:
+#	print("Input given is not in tuple")
